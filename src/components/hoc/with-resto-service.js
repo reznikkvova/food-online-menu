39bf0283bac86b1fa99adc07cba905ccfb5fb08e
Vova_Reznik <reznikvova2222@gmail.com>
2021-02-27 17:43:02 +0200

    github pull

---
+++ src/components/hoc/with-resto-service.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import RestoServiceContext from '../resto-service-context';
+
+
+const WithRestoService = () => (Wrapped) => {
+    return(props) => {
+       return (
+          <RestoServiceContext.Consumer>
+               {
+                  (RestoService) => {
+                     return <Wrapped {...props} RestoService={RestoService}/>
+                  }
+               }
+          </RestoServiceContext.Consumer>
+       )
+    }
+};
+
+export default WithRestoService;
